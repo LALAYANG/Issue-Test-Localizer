be613616b32244d66f1111c931caf87c76a7761d
Yang Chen <yangc9@illinois.edu>
2025-08-10 13:07:05 -0500

    refactor model prompting

--- src/model_config.py
+++ src/model_config.py
@@ -1,18 +1,35 @@
 import os
-
 import openai
+import anthropic
 
 
-def prompt_model(model_name, prompt, temperature=0.7):
-    client = openai.OpenAI(
-        api_key=os.environ["OPENAI_API_KEY"],
-        base_url=os.environ["MODEL_SERVING_URL"],
-    )
+def prompt_model(model_name, prompt, temperature=0.8):
+    print(f"Using model: {model_name}; temperature: {temperature}")
+    if "claude" in model_name.lower():
+        client = anthropic.Anthropic(
+            api_key=os.environ["OPENAI_API_KEY"],
+        )
 
-    response = client.chat.completions.create(
-        model=model_name,
-        temperature=temperature,
-        messages=[{"role": "user", "content": prompt}],
-    )
-    print(response)
+        response = client.messages.create(
+            model=model_name,
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=4000,
+            temperature=temperature,
+        )
+        print(response)
+        return response.content[0].text
+    else:
+        client = openai.OpenAI(
+            api_key=os.environ["OPENAI_API_KEY"],
+            base_url=os.environ["MODEL_SERVING_URL"],
+        )
+
+        response = client.chat.completions.create(
+            model=model_name,
+            temperature=temperature,
+            messages=[{"role": "user", "content": prompt}],
+        )
+        print(response)
     return response.choices[0].message.content.strip()
